a42dc60dd6960229adfad36b35032f469d90b926
Ricardo Lima <ricardous27@gmail.com>
2022-08-18 19:56:23 -0300

    exercicio 1 a 3

---
+++ Fundamentos/Bloco-4/dia-1-JS/script.js
@@ -0,0 +1,46 @@
+//exercício 1:
+
+const myName = "Ricardo Lima";
+const birthCity = "Aracaju";
+let birthYear = 1992;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+
+console.log(birthCity);
+
+//exercício 2:
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+
+// exercicio 3:
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log (area);
+
+
+
