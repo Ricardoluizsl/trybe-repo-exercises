1d91756a4531ef055e9be9f722520a79805a5ad1
Ricardo Lima <ricardous27@gmail.com>
2022-08-19 09:26:20 -0300

    exercicios

--- Fundamentos/Bloco-4/dia-1-JS/script2.js
+++ Fundamentos/Bloco-4/dia-1-JS/script2.js
@@ -1,17 +1,1 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
-
-typeof patientId;
-
-console.log(typeof patientId);
-console.log(typeof isEnrolled);
-console.log(typeof patientInfo);
-console.log(typeof patientEmail);
-
-
 
